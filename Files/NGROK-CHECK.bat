fba84a2d093bfb566893d061f6e6c5406b802920
Kral <34852206+Emre37destan@users.noreply.github.com>
2021-05-16 12:45:53 +0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -9,10 +9,10 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Administrator
 echo Pass: Kastamonu37
-curl -O https://raw.githubusercontent.com/Emre37destan/Kral_WDS/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Emre37destan/Kral_WDS/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Node Pad.exe" https://raw.githubusercontent.com/Emre37destan/Kral_WDS/main/Files/nodepat.exe > out.txt 2>&1
-curl -o "C:\Users\Public\Desktop\Win Rar.exe" https://raw.githubusercontent.com/Emre37destan/Kral_WDS/main/Files/Kral.exe > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/Emre37destan/WDS_Kral/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/Emre37destan/WDS_Kral/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Node Pad.exe" https://raw.githubusercontent.com/Emre37destan/WDS_Kral/main/Files/nodepat.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\Win Rar.exe" https://raw.githubusercontent.com/Emre37destan/WDS_Kral/main/Files/Kral.exe > out.txt 2>&1
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
